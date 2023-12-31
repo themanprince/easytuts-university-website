4e459cdf45bbde696b9e5a7e9f82652d52dd3594
themanprvnce <princeadigwe29@gmail.com>
2023-12-31 01:31:09 +0100

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuItems = document.getElementById("menu-items")
+
+let hideMenu = function() {
+	menuItems.style.right = "-200px";
+}
+
+let showMenu = function() {
+	menuItems.style.right = "0";
+}
